0dbb0849e059704d1a56fe1d6b57ba4ac6b81c20
nishantsikarwar <nishant.sikarwar007@gmail.com>
2017-02-16 19:43:38 +0530

    Add files via upload

--- Nearest Fabonacci.cpp
+++ Nearest Fabonacci.cpp
@@ -4,16 +4,34 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-   int i=0;
-	int p=0;
- int f (int n)
+  long long  int i=0;
+//	int p=0;
+ inline long long int f(int n)
 {
-	if(n==0||n==1)
-	return n;
-	else return f(n-1)+f(n-2);
+
+if (n==0||n==1)
+return n;
+ 
+	else
+	{
 	
+		 if (n%2==0)
+		{
+			n=n/2;
+		return (f(n)*f(n)+2*f(n)*f(n-1));
+		
+	        }
+		else 
+		{
+			n=(n-1)/2;
+			return f(n+1)*f(n+1)+f(n)*f(n);
+			
+		 } 
+		
 }
-int  nef (int n)
+
+}
+long long int  nef (int n)
 {
 	
 	if(n==1)
@@ -26,30 +44,31 @@ int  nef (int n)
       	return nef(n-f(i));
          }
 }
- int fr (int n)
+ /*int fr (int n)
 {
-	if(n==1||n==0)
-	
-	return n;
+	if(n==1||n==0)	
+	return p+n;
     
-    //else if (n<=0)
-  //  {
-    //	return 0;
+ //   else if (n<=0)
+   // {
+    //	n= -n;
+    //	return fr(n);
 	//}
 	else 
 	{
-		
+		p+=1;
 		n=n-nef(n);
-		return fr(n)+1;
+		return fr(n);
 	}
 	
-}
-int main() {
+}*/
+int main() 
+{
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-unsigned int n;
+long long int n;
 	cin>>n;
-	cout <<fr(n);
+	cout <<nef(n);
 /*	int i=1;
 	while (n)
 	{6
